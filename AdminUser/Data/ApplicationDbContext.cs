58993130ff885760047f19c925bef5480c7d6a11
KristiyanV <kris20003@abv.bg>
2022-03-02 13:26:06 +0200

    add

---
+++ AdminUser/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using AdminUser.Domain;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using AdminUser.Models;
+
+namespace AdminUser.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+            this.Database.EnsureCreated();
+        }
+        public DbSet<AdminUser.Models.ClientBindingAllViewModel> ClientBindingAllViewModel { get; set; }
+    }
+}
